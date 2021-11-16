433dd17f62b054d0402f21b04ee011788ba7ca50
bornedj <bornedj@miamioh.edu>
2021-11-16 16:41:34 -0500

    connected the index.ts file to the postgres database

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,16 @@
+const { Pool } = require('pg');
+const pgSetup = async () => {
+    const pool = new Pool({
+        user: "postgres",
+        host: "localhost",
+        password: "postgres",
+        database: "ecommerce",
+        port: 5432
+    });
+    const res = await pool.query('SELECT NOW()');
+    await pool.end();
+    console.log(res);
+};
+pgSetup();
 console.log('Hello world');
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
+//imports
+const { Pool } = require('pg')
+
+//setting up the connection with postgres
+const pgSetup: any = async () => {
+    const pool = new Pool({
+        user: "postgres",
+        host: "localhost",
+        password: "postgres",
+        database: "ecommerce",
+        port: 5432
+    });
+
+    const res = await pool.query('SELECT NOW()');
+    await pool.end();
+    console.log(res);
+}
+
+pgSetup();
+
 console.log('Hello world');
