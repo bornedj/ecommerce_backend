dd5f9c98f96d646324e49e25fff20b147ec2c912
bornedj <bornedj@miamioh.edu>
2021-11-17 12:02:51 -0500

    updating main with the correct login

--- dist/routes/login.js
+++ dist/routes/login.js
@@ -4,14 +4,5 @@ const db = require('../db');
 const loginRouter = require('express').Router();
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
-passport.use(new LocalStrategy({}, {
-    try: {}, catch(err) {
-        return done(err);
-    }
-}));
-loginRouter.post('/', (req, res, next) => {
-    console.log("at login");
-    res.send('at login');
-});
 module.exports = loginRouter;
 //# sourceMappingURL=login.js.map
